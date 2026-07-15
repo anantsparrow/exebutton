ad6f6334ce59d4422eafd79574d9feec7c6b58ee
anantsparrow <anantsparrow@users.noreply.github.com>
2026-07-15 11:59:39 +0530

    Update webhook URL to new provided URL

--- PhishingReporterAddIn/ConfigManager.cs
+++ PhishingReporterAddIn/ConfigManager.cs
@@ -32,7 +32,7 @@ namespace PhishingReporterAddIn
                 {
                     var defaultConfig = new PhishingConfig
                     {
-                        ApiUrl = "https://webhook.site/fba8fef5-2971-48a3-9422-22bb26bba0dd",
+                        ApiUrl = "https://webhook.site/e0cc9551-e7d5-4704-b0c2-ef4874a502d8",
                         TimeoutSeconds = 30
                     };
                     Save(defaultConfig);
@@ -54,7 +54,7 @@ namespace PhishingReporterAddIn
                 // Return default fallback configuration
                 return new PhishingConfig
                 {
-                    ApiUrl = "https://webhook.site/fba8fef5-2971-48a3-9422-22bb26bba0dd",
+                    ApiUrl = "https://webhook.site/e0cc9551-e7d5-4704-b0c2-ef4874a502d8",
                     TimeoutSeconds = 30
                 };
             }
